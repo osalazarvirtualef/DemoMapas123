0010a2631e9392ce0d69ce25ac853723a35bfa32
Osvaldo Salazar Manilla <osalazar@csgit.com.mx>
2015-04-21 19:26:27 -0500

    jhyt

---
+++ src/com/demomapas/Key2.java
@@ -0,0 +1,201 @@
+package com.demomapas;
+
+import com.virtualef.pgj.service.commandmentService.model.Key;
+
+public class Key2 {
+	  @com.google.api.client.util.Key
+	  private java.lang.String appId;
+
+	  /**
+	   * The value may be {@code null}.
+	   */
+	  @com.google.api.client.util.Key
+	  private java.lang.Boolean complete;
+
+	  /**
+	   * The value may be {@code null}.
+	   */
+	  @com.google.api.client.util.Key @com.google.api.client.json.JsonString
+	  private java.lang.Long id;
+
+	  /**
+	   * The value may be {@code null}.
+	   */
+	  @com.google.api.client.util.Key
+	  private java.lang.String kind;
+
+	  /**
+	   * The value may be {@code null}.
+	   */
+	  @com.google.api.client.util.Key
+	  private java.lang.String name;
+
+	  /**
+	   * The value may be {@code null}.
+	   */
+	  @com.google.api.client.util.Key
+	  private java.lang.String namespace;
+	  
+	  
+	  		public class Key{
+	  		  @com.google.api.client.util.Key
+	  		  private java.lang.String appId;
+
+	  		  /**
+	  		   * The value may be {@code null}.
+	  		   */
+	  		  @com.google.api.client.util.Key
+	  		  private java.lang.Boolean complete;
+
+	  		  /**
+	  		   * The value may be {@code null}.
+	  		   */
+	  		  @com.google.api.client.util.Key @com.google.api.client.json.JsonString
+	  		  private java.lang.Long id;
+
+	  		  /**
+	  		   * The value may be {@code null}.
+	  		   */
+	  		  @com.google.api.client.util.Key
+	  		  private java.lang.String kind;
+
+	  		  /**
+	  		   * The value may be {@code null}.
+	  		   */
+	  		  @com.google.api.client.util.Key
+	  		  private java.lang.String name;
+
+	  		  /**
+	  		   * The value may be {@code null}.
+	  		   */
+	  		  @com.google.api.client.util.Key
+	  		  private java.lang.String namespace;
+
+	  		  /**
+	  		   * The value may be {@code null}.
+	  		   */
+	  		  @com.google.api.client.util.Key
+	  		  private Key parent;
+
+			public java.lang.String getAppId() {
+				return appId;
+			}
+
+			public void setAppId(java.lang.String appId) {
+				this.appId = appId;
+			}
+
+			public java.lang.Boolean getComplete() {
+				return complete;
+			}
+
+			public void setComplete(java.lang.Boolean complete) {
+				this.complete = complete;
+			}
+
+			public java.lang.Long getId() {
+				return id;
+			}
+
+			public void setId(java.lang.Long id) {
+				this.id = id;
+			}
+
+			public java.lang.String getKind() {
+				return kind;
+			}
+
+			public void setKind(java.lang.String kind) {
+				this.kind = kind;
+			}
+
+			public java.lang.String getName() {
+				return name;
+			}
+
+			public void setName(java.lang.String name) {
+				this.name = name;
+			}
+
+			public java.lang.String getNamespace() {
+				return namespace;
+			}
+
+			public void setNamespace(java.lang.String namespace) {
+				this.namespace = namespace;
+			}
+
+			public Key getParent() {
+				return parent;
+			}
+
+			public void setParent(Key parent) {
+				this.parent = parent;
+			}
+	  		  
+	  		  
+	  			
+	  		}
+
+
+			public java.lang.String getAppId() {
+				return appId;
+			}
+
+
+			public void setAppId(java.lang.String appId) {
+				this.appId = appId;
+			}
+
+
+			public java.lang.Boolean getComplete() {
+				return complete;
+			}
+
+
+			public void setComplete(java.lang.Boolean complete) {
+				this.complete = complete;
+			}
+
+
+			public java.lang.Long getId() {
+				return id;
+			}
+
+
+			public void setId(java.lang.Long id) {
+				this.id = id;
+			}
+
+
+			public java.lang.String getKind() {
+				return kind;
+			}
+
+
+			public void setKind(java.lang.String kind) {
+				this.kind = kind;
+			}
+
+
+			public java.lang.String getName() {
+				return name;
+			}
+
+
+			public void setName(java.lang.String name) {
+				this.name = name;
+			}
+
+
+			public java.lang.String getNamespace() {
+				return namespace;
+			}
+
+
+			public void setNamespace(java.lang.String namespace) {
+				this.namespace = namespace;
+			}
+	  		
+
+}
